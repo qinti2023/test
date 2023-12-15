99c0403f273bd6fe0862e91f438b99c6c8f1017c
qinti <142979556+qinti2023@users.noreply.github.com>
2023-12-16 03:48:14 +0800

    Add files via upload

---
+++ finalexam2023 answer template.R
@@ -0,0 +1,78 @@
+# I certify that I have complied with the University of Pennsylvania's [Code of Academic Integrity]
+# (https://catalog.upenn.edu/pennbook/code-of-academic-integrity/) in completing this final exam.
+
+
+### 1. Setup the data, load the libraries, and inspect the data
+
+# 1a. (2 points)
+
+# 1b. (2 points)
+
+# 1c. (2 points)
+
+# 1d. (2 points)
+
+# 1e. (2 points)
+
+# 1f. (3 points)
+
+# 1g. (3 points)
+
+# 1h. (3 points)
+
+# 1i. (3 points)
+
+
+### 2. Create a New Dataset, Fix Data Errors, and Wrangle the Data
+
+# 2a. (4 points)
+
+# 2b. (3 points)
+
+# 2c. (3 points)
+
+# 2d. (3 points)
+
+# 2e. (4 points)
+
+# 2f. (4 points)
+
+
+### 3. Working with Dates and Times
+
+# 3a. (2 points)
+
+# 3b. (4 points)
+
+# 3c. (3 points)
+
+# 3d. (5 points) 
+
+# 3e. (6 points)
+
+# 3f. (4 points)
+
+# 3g. (4 points)
+
+
+### 4. Maps
+
+# 4a. (2 points)
+
+# 4b. (4 points)
+
+# 4c. (4 points)
+
+# 4d. (4 points)
+
+# 4e. (3 points)
+
+# 4f. (4 points)
+
+# 4g. (4 points)
+
+# 4h. (4 points)
+
+# 4i. (4 points)
+
+# 4j. (5 points)
